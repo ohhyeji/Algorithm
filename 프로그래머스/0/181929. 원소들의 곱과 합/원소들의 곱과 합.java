c10dc6dcef57ab09b9ab11029fd55b08f735b377
ohhyeji <132237910+ohhyeji@users.noreply.github.com>
2024-02-16 00:03:46 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int sum = 0;
+        int multi = 1;
+        
+        for(int i=0; i<num_list.length; i++){
+            multi *= num_list[i];
+            sum += num_list[i];
+        }
+        
+        sum = sum*sum;
+        
+        if(sum > multi){
+            answer = 1;
+        }else{
+            answer = 0;
+        }
+        
+        return answer;
+    }
+}
