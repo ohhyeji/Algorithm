f8329f2c633efababf74d0d79bd32985c452a222
ohhyeji <132237910+ohhyeji@users.noreply.github.com>
2024-02-16 00:02:19 +0900

    [level 0] Title: 더 크게 합치기, Time: 8.19 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        if(Integer.parseInt(a+""+b) > Integer.parseInt(b+""+a)){
+            answer = Integer.parseInt(a+""+b);
+        }else{
+            answer = Integer.parseInt(b+""+a);
+        }
+        
+        return answer;
+    }
+}
