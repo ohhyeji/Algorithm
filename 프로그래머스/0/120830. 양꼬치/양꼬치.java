474c6db75a2b2e1aea3f311ea0e189816f1b773c
ohhyeji <132237910+ohhyeji@users.noreply.github.com>
2024-02-15 23:54:00 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        if(n < 10){
+            answer = (n*12000) + (k*2000);
+        }else {
+            int num = n / 10;
+            k = k - num;
+            answer = (n*12000) + (k*2000);
+        }
+        
+        return answer;
+    }
+}
