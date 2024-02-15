4752224c66844da5d2d284ac6e440647c405a5ae
ohhyeji <132237910+ohhyeji@users.noreply.github.com>
2024-02-16 00:01:41 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        
+        for(int i=0; i<numbers.length; i++){
+            if(n >= answer){
+                answer += numbers[i];
+            }
+        }
+        
+        return answer;
+    }
+}
