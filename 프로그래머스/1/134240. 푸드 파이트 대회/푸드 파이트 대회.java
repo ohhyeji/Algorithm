4054fd8062cf397c86a1a7a0b8fca4e03b0fe3f3
ohhyeji <132237910+ohhyeji@users.noreply.github.com>
2024-02-20 00:01:44 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 13.79 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        
+        for(int i=1; i<food.length; i++){
+            if(food[i]%2==0){
+                for(int j=0; j<food[i]/2; j++){
+                    answer += i;
+                }
+            }else{
+                for(int j=0; j<food[i]/2; j++){
+                    answer += i;
+                }
+            }
+        }
+        answer += 0;
+        
+        for(int i=answer.length()-2; i>=0; i--){
+            answer += String.valueOf(answer.charAt(i));
+        }
+        
+        return answer;
+    }
+}
