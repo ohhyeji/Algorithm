93f67b0d656462f48c3ffbcdcdcd48616f93f83f
ohhyeji <132237910+ohhyeji@users.noreply.github.com>
2024-02-22 18:32:06 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int chicken) {
+        int answer = 0;
+        int coupon = chicken;
+        
+        
+        while(coupon >= 10){
+            int leftcoupon = coupon%10;
+            answer += coupon/10;
+            coupon = leftcoupon + (coupon/10);
+        }
+        
+        return answer;
+    }
+}
